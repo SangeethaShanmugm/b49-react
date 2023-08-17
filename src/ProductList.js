2a1016f975df801bf217dc3532e6f2a1f0ae949e
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-08-17 12:12:30 +0530

    updated

--- src/ProductList.js
+++ src/ProductList.js
@@ -4,10 +4,18 @@ import { INITIAL_PRODUCT_LIST } from "./App";
 export function ProductList() {
   const productList = INITIAL_PRODUCT_LIST;
   return (
-    <div className="product-list">
-      {productList.map((pd, index) => (
-        <Product key={index} product={pd} id={index} />
-      ))}
+    <div>
+      <div className="product-list">
+        {productList.map((pd, index) => (
+          <Product key={index} product={pd} id={index} />
+        ))}
+      </div>
     </div>
   );
 }
+
+//Task  - 20 mins
+//Add products
+// 6 field input box for(name,price,
+//poster, category,rating,description)
+// Add product button
